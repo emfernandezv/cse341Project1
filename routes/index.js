cf8cdc1d5dfe68b5fb64ac6cabb1ab8b30e9543d
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2024-03-11 13:19:08 -0600

    database connection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const usersRoutes = require('./users');
 
 router.get('/', (req, res) => {res.send('Hello world');});
+
+router.use('/users', usersRoutes);
 
 module.exports = router;
