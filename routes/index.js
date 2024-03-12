378abe001a5dde50f977a850d9e8299d3ddc6c74
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2024-03-11 19:09:06 -0600

    adding swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const usersRoutes = require('./users');
+const swaggerRoutes = require('./swagger');
 
-router.get('/', (req, res) => {res.send('Hello world');});
+router.use('/', swaggerRoutes);
+
+router.get("/", (req, res) => {
+    //#swagger.tags=['Hello World]
+    res.send("Hello world");
+});
 
 router.use('/users', usersRoutes);
 
